c1bd9198450d13603dfde33cdcf8558b4bfc154e
Alexander Kerner <alexkerner1000@gmail.com>
2023-01-25 12:04:40 +0100

    Continue

--- src/main/java/drrename/commons/CommandRunner.java
+++ src/main/java/drrename/commons/CommandRunner.java
@@ -6,9 +6,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 public class CommandRunner {
+
+    public void runCommand(List<String> command) {
+        runCommand(command.toArray(new String[0]));
+    }
 
     public void runCommand(String[] command) {
         log.debug("Running {}", Arrays.toString(command));
